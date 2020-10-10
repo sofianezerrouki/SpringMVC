f82c12c5f3794f6b86f5a5edc4264517004dbfc2
sofiane <sofianeyooung@gmail.com>
2020-10-10 10:49:36 +0100

    add a requestMpping method for the process of Data

--- src/com/camacode/spring/HelloWorldController.java
+++ src/com/camacode/spring/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.camacode.spring;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +16,22 @@ public class HelloWorldController {
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
-		
-		
 		return "helloWorld";
 	}
 	
+	@RequestMapping("processFormVersionTwo")
+	public String letsShoutDout(HttpServletRequest request,Model model) {
+		//read the request parameters
+		String nameStudent = request.getParameter("studentName");
+		//convert the data to upper case 
+		nameStudent = nameStudent.toUpperCase();
+		//create the message 
+		String msg = "Hi ! "+nameStudent;
+		//add the message to the Model 
+		model.addAttribute("message",msg);
+		
+		
+		//hello world is the Name of the jsp File 
+		return "helloWorld";
+	}
 }
